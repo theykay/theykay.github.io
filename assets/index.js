712d69c698185fce18b908d5847f94fd7d68f53d
TheyKay <murraykyleb@gmail.com>
2020-09-15 22:43:00 -0600

    added resume

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,2 @@
-let rModal = document.getElementById('resumeModal');
-let rButton = document.getElementById('resumeBtn');
-let rClose = document.getElementById('close');
-
-rButton.addEventListener('click', function() {
-    rModal.style.display = "block";
-});
-
-rClose.addEventListener('click', function() {
-    rModal.style.display = "none";
-});
-
-window.addEventListener('click', function() {
-    rModal.style.display = "none";
-});
 
 
